821ced4aa480fc019bb0dca520b83f494273c8f8
vynas <hamdabousnina@gmail.com>
2025-05-26 10:17:05 -0400

    render data

--- tasks/src/Items.jsx
+++ tasks/src/Items.jsx
@@ -1,8 +1,19 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
+
+const Items = () => {
+  const { isPending, data } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch("/"),
+  });
+
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
